c399a2f6d34df13d203d20b26e6e480d167b3b9d
frankaging <zxw0424@gmail.com>
2021-09-29 22:32:52 -0700

    minor change

--- codes/models/seq2seq/run_evaluation_encoder.py
+++ codes/models/seq2seq/run_evaluation_encoder.py
@@ -406,7 +406,7 @@ def predict_and_save(
     cfg = locals().copy()
 
     # read-in datasets
-    test_data, _ = dataset.get_dual_dataset(novel_attribute=True, restrict_sampling=restrict_sampling)
+    test_data, _ = dataset.get_dual_dataset(novel_attribute=True, restrict_sampling=cfg["kwargs"]["restrict_sampling"])
     data_iterator = DataLoader(test_data, batch_size=1, shuffle=False)
     eval_max_decoding_steps = max_decoding_steps
     with open(output_file_path, mode='w') as outfile:
